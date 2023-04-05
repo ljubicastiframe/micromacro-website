eae52aa47e848acecb4228d631deda685bc6b1e9
Ljubicasti <60853843+ljubicastiframe@users.noreply.github.com>
2023-04-05 19:23:18 +0200

    cart progres

---
+++ main.js
@@ -0,0 +1,150 @@
+
+let cartIcon = document.querySelector('#cart-icon')
+let cart = document.querySelector('.cart')
+let cartClose = document.querySelector('#close-cart')
+
+window.addEventListener("scroll", function(){
+    var meni = document.querySelector("nav");
+    meni.classList.toggle("sticky", window.scrollY > 0);
+})
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+cartIcon.onclick = () =>{
+    cart.classList.add("active");
+}
+
+cartClose.onclick = () =>{
+    cart.classList.remove("active");
+}
+
+
+if (document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+}else{
+    ready();
+}
+
+function ready(){
+    //Uklanjanje proizvoda iz korpe
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i]
+        button.addEventListener("click", removeCartItem)
+    }
+    //Menjanje kolicine
+    var quantityInputs = document.getElementsByClassName("cart-quantity");
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+    }
+
+    var addCart = document.getElementsByClassName("add-cart");
+    for (var i=0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener("click", addCartClicked);
+    }
+    //Funkcionalnost kupi dugmeta
+    document
+    .getElementsByClassName('btn-buy')[0]
+    .addEventListener("click", buyButtonClicked);
+}
+
+//Kupi dugme
+function buyButtonClicked(){
+    alert('Vasa porudzbina je poslata');
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
+}
+
+//Uklanjanje proizvoda iz korpe
+function removeCartItem(){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+//Menjanje kolicine
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updatetotal();
+}
+
+//Dodaj u korpu
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+
+}
+
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title) {
+            alert("Ovaj proizvod ste vec dodali u korpu");
+            return;
+        }
+    }
+    var cartBoxContent = `
+                            <img src="${productImg}" alt="" class="cart-img">
+                            <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                                <input type="number" value="1" class="cart-quantity">
+                            </div>
+                            <i class="cart-remove">remove</i>`;
+    cartShopBox.innerHTML = cartBoxContent
+    cartItems.append(cartShopBox)
+    cartShopBox.getElementsByClassName('cart-remove')[0]
+        .addEventListener('click', removeCartItem);
+    cartShopBox.getElementsByClassName('cart-quantity')[0]
+        .addEventListener('change', quantityChanged);
+
+}
+
+//Update totala
+function updatetotal(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+    for(var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innerText.replace("RSD", ""));
+        var quantity = quantityElement.value;
+        total = total + price * quantity;
+    }
+    //zaokruzivanje na dve decimale
+    total = Math.round(total * 100) / 100;
+
+    document.getElementsByClassName("total-price")[0].innerText = total + "RSD";
+}
